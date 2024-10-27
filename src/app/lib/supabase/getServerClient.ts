953341e0e79aa1d764bdfb5fad0681086976aab8
maru0210 <m.ryuto0210@gmail.com>
2024-10-28 05:11:54 +0900

    リダイレクト処理を組み込んだgetServerClient関数を作成

---
+++ src/app/lib/supabase/getServerClient.ts
@@ -0,0 +1,15 @@
+"use server"
+
+import {createClient} from "@/app/utils/supabase/server";
+import {redirect} from "next/navigation";
+
+export default async function getServerClient() {
+  const supabase = await createClient()
+
+  const {data, error} = await supabase.auth.getUser()
+  if(error || !data?.user) {
+    redirect('/login')
+  }
+
+  return supabase
+}
