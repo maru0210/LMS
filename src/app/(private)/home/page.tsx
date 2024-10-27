edc66f8350271c1b18630521fb8a4aa56e7a1476
maru0210 <m.ryuto0210@gmail.com>
2024-10-28 05:23:01 +0900

    ホームページを編集

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,26 +1,23 @@
-import {createClient} from "@/app/utils/supabase/server";
-import {redirect} from "next/navigation";
 import {logout} from "@/app/lib/supabase/auth";
-import Navigation from "@/app/(private)/home/Navigation";
+import Navigation from "@/app/(private)/components/Navigation";
+import getServerClient from "@/app/lib/supabase/getServerClient";
 
-export default async function Dashboard() {
-  const supabase = await createClient()
+export default async function Home() {
+  const supabase = await getServerClient()
 
-  const {data, error} = await supabase.auth.getUser()
-  if(error || !data?.user) {
-    redirect('/login')
-  }
-
-  const profile = await supabase.from('profile').select()
-  console.log(profile.data?.at(0))
-
+  const profile = await supabase.from('profiles').select()
   const name = profile.data?.at(0).name as string;
 
   return (
     <Navigation>
-      <h1>Dashboard</h1>
-      <p>{name}:{data.user.email}</p>
-      <button onClick={logout}>ログアウト</button>
+      <div>
+        <div className="w-full rounded-lg px-4 py-2.5 border-2 border-orange-100 bg-orange-50">
+          <p className="text-lg">最後に学習したもの</p>
+          <p className="mx-2.5 my-1">2-10 アルゴリズムとデータ構造</p>
+        </div>
+      </div>
+      {/*<p>{name}:{data.user.email}</p>*/}
+      {/*<button onClick={logout}>ログアウト</button>*/}
     </Navigation>
   )
 }
