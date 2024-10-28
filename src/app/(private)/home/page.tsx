9cb540eec48cc6c10988c0e0953906da7f99369f
maru0210 <m.ryuto0210@gmail.com>
2024-10-28 11:47:10 +0900

    microCMSのテスト

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,6 +1,7 @@
 // import {logout} from "@/app/lib/supabase/auth";
 import Navigation from "@/app/(private)/components/Navigation";
 import getServerClient from "@/app/lib/supabase/getServerClient";
+import {getList} from "@/app/lib/microCMS/microcms";
 
 export default async function Home() {
   const {supabase, user} = await getServerClient()
@@ -8,15 +9,24 @@ export default async function Home() {
   const profile = await supabase.from('profiles').select()
   const name = profile.data?.at(0).name as string;
 
+  const list = await getList();
+
   return (
     <Navigation>
       <div>
-        <div className="w-full rounded-lg px-4 py-2.5 border-2 border-orange-100 bg-orange-50">
-          <p className="text-lg">最後に学習したもの</p>
-          <p className="mx-2.5 my-1">2-10 アルゴリズムとデータ構造</p>
+        <div className="w-full rounded-lg px-4 py-2.5 space-y-2 border-2 border-orange-100 bg-orange-50">
+          <p className="text-lg">最後に学習したコンテンツ</p>
+          <p className="">2-10 アルゴリズムとデータ構造</p>
         </div>
       </div>
-      <p>{name}:{user.email}</p>
+
+      <div>
+        {list.contents.map((item, index) => (
+          <div>
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
       {/*<button onClick={logout}>ログアウト</button>*/}
     </Navigation>
   )
