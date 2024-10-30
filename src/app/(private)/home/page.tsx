c425ee83618b40f07d43004560f2897b6058ccab
maru0210 <m.ryuto0210@gmail.com>
2024-10-30 18:12:20 +0900

    Homeを更新

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,41 +1,41 @@
-// import {logout} from "@/app/lib/supabase/auth";
+import {logout} from "@/app/lib/supabase/auth";
 import Navigation from "@/app/(private)/components/Navigation";
-// import getServerClient from "@/app/lib/supabase/getServerClient";
+import getServerClient from "@/app/lib/supabase/getServerClient";
 import {getChapters, getList} from "@/app/lib/microCMS/microcms";
+import Profile from "@/app/(private)/profile/page";
+import ChapterCard from "@/app/(private)/home/ChapterCard";
 
 export default async function Home() {
-  // const {supabase} = await getServerClient()
+  const {supabase} = await getServerClient()
 
-  // const profile = await supabase.from('profiles').select()
-  // const name = profile.data?.at(0).name as string;
+  const profile = await supabase.from('profiles').select()
+  const name = profile.data?.at(0).name as string;
 
-  const chapters = await getChapters({orders: "number"});
-  const list = await getList({orders: "section"});
+  const {contents: chapters} = await getChapters({orders: "number"});
 
   return (
     <Navigation>
-      <div>
-        <div className="w-full rounded-lg px-4 py-2.5 space-y-2 border-2 border-orange-100 bg-orange-50">
-          <p className="text-lg">最後に学習したコンテンツ</p>
-          <p className="">2-10 アルゴリズムとデータ構造</p>
-        </div>
-      </div>
+      <div className="size-full">
+        <div className="flex flex-col gap-8">
+          <div>
+            <div className="w-full rounded-lg p-4 space-y-2 border-0 border-lime-100 bg-lime-50 shadow">
+              <p className="">最後に学習したコンテンツ</p>
+              <p className="text-sm">ここに最後に学習したコンテンツが入ります</p>
+            </div>
+          </div>
 
-      <div className="flex flex-col gap-8">
-        {chapters.contents.map((chapter, index) => (
-          <div key={index}>
-            <p>{chapter.number + "章 " + chapter.title}</p>
-            {list.contents.filter(
-              item => item.chapter.number === chapter.number
-            ).map((item, index) => (
-              <div key={index}>
-                {item.chapter.number + "-" + item.section + ": " + item.title}
-              </div>
+          <div className="flex flex-wrap gap-8">
+            {chapters.map(chapter => (
+              <ChapterCard chapter={chapter} key={chapter.id}/>
+            ))}
+
+            {chapters.map(chapter => (
+              <ChapterCard chapter={chapter} key={chapter.id}/>
             ))}
           </div>
-        ))}
+        </div>
       </div>
-      {/*<button onClick={logout}>ログアウト</button>*/}
+      <button onClick={logout}>ログアウト</button>
     </Navigation>
   )
 }
