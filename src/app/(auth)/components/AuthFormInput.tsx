d1f38138eeabad129779057ef92ef462d16bc54d
maru0210 <m.ryuto0210@gmail.com>
2024-10-30 23:52:22 +0900

    値を保持する機能を追加

--- src/app/(auth)/components/AuthFormInput.tsx
+++ src/app/(auth)/components/AuthFormInput.tsx
@@ -1,17 +1,37 @@
-import {HTMLInputAutoCompleteAttribute} from "react";
+import {HTMLInputAutoCompleteAttribute, useState} from "react";
+
+type Props = {
+  label: string;
+  id: string;
+  autoComplete: HTMLInputAutoCompleteAttribute;
+  keepValue?: boolean;
+  defaultValue?: string;
+}
 
 export default function AuthFormInput(
-  props: {label: string, id: string, autoComplete: HTMLInputAutoCompleteAttribute | undefined},
+  {label, id, autoComplete, keepValue = true, defaultValue = ""} : Props,
 ) {
-  const {label, id, autoComplete} = props;
+  const [state, setState] = useState(defaultValue)
 
   return (
     <div className="space-y-2">
       <label htmlFor={id} className="ml-0.5">{label}</label>
-      <input
-        id={id} name={id} type={id} required autoComplete={autoComplete}
-        className="block w-full rounded-lg border-0 px-4 py-2.5 shadow-sm ring-2 ring-inset ring-gray-200 focus:ring-2 focus:ring-indigo-800"
-      />
+
+      {keepValue
+        ? (
+          <input
+            id={id} name={id} type={id} required autoComplete={autoComplete}
+            value={state} onChange={(e) => setState(e.currentTarget.value)}
+            className="block w-full rounded-lg border-0 px-4 py-2.5 shadow-sm ring-2 ring-inset ring-gray-200 focus:ring-2 focus:ring-indigo-800"
+          />
+        )
+        : (
+          <input
+            id={id} name={id} type={id} required autoComplete={autoComplete}
+            className="block w-full rounded-lg border-0 px-4 py-2.5 shadow-sm ring-2 ring-inset ring-gray-200 focus:ring-2 focus:ring-indigo-800"
+          />
+        )
+      }
     </div>
   )
 }
