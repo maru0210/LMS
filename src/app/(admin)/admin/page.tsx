5acc19267635286fa9b23feccd9242fa90ae336f
maru0210 <m.ryuto0210@gmail.com>
2024-11-01 22:48:00 +0900

    コンテンツ管理画面を追加

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,25 @@
+import Navigation from "@/app/components/Navigation";
+import {authAdmin, logout} from "@/app/lib/supabase/auth";
+import ContentsManager from "@/app/(admin)/admin/ContentsManager";
+import {getCmsContents, getDbContents} from "@/app/(admin)/admin/manageContents";
+
+export default async function Admin() {
+  await authAdmin()
+
+  return (
+    <Navigation>
+      <div className="flex flex-col gap-16">
+        <h1 className="text-2xl">コンテンツ管理</h1>
+
+        <ContentsManager
+          defaultDbContents={await getDbContents()}
+          defaultCmsContents={await getCmsContents()}
+        />
+
+        <div>
+          <button onClick={logout}>ログアウト</button>
+        </div>
+      </div>
+    </Navigation>
+  )
+}
