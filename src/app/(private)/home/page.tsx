ae195f07c283eb5179097d85dbd09b2217949137
maru0210 <m.ryuto0210@gmail.com>
2024-11-01 22:54:07 +0900

    ホーム画面を更新

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,40 +1,44 @@
-import {logout} from "@/app/lib/supabase/auth";
-import Navigation from "@/app/(private)/components/Navigation";
-import getServerClient from "@/app/lib/supabase/getServerClient";
-import {getChapters} from "@/app/lib/microCMS/microcms";
-import Profile from "@/app/(private)/profile/page";
+import {auth} from "@/app/lib/supabase/auth";
+import Navigation from "@/app/components/Navigation";
 import ChapterCard from "@/app/(private)/home/ChapterCard";
+import {createClient} from "@/app/utils/supabase/server";
 
 export default async function Home() {
-  const {supabase} = await getServerClient()
+  await auth()
 
-  const profile = await supabase.from('profiles').select()
-  const name = profile.data?.at(0).name as string;
+  const supabase = await createClient()
 
-  const {contents: chapters} = await getChapters({orders: "number"});
+  // const profile = await supabase.from('profiles').select()
+  // const name = profile.data?.at(0).name as string;
+
+  const contents = await supabase.from("contents").select().order("chapter")
+
+  const chapters = Array.from(
+    new Map(contents.data?.map((content) => [content.chapter, content.chapter as number])).values()
+  );
 
   return (
     <Navigation>
-        <div className="flex flex-col gap-8">
-          <div>
-            <div className="w-full rounded-lg p-4 space-y-2 border-0 border-lime-100 bg-lime-50 shadow">
-              <p className="">前回の続き</p>
-              <p className="text-sm">ここに最後に学習したコンテンツが入ります</p>
-            </div>
-          </div>
-
-          <h1 className="text-lg">学習コンテンツ</h1>
-
-          <div className="flex flex-wrap gap-8">
-            {chapters.map(chapter => (
-              <ChapterCard chapter={chapter} key={chapter.id}/>
-            ))}
-
-            {chapters.map(chapter => (
-              <ChapterCard chapter={chapter} key={chapter.id}/>
-            ))}
+      <div className="flex flex-col gap-8">
+        <div>
+          <div className="w-full rounded-lg p-4 space-y-2 border-0 border-lime-100 bg-lime-50 shadow">
+            <p className="">前回の続き</p>
+            <p className="text-sm">ここに最後に学習したコンテンツが入ります</p>
           </div>
         </div>
+
+        <h1 className="text-lg">学習コンテンツ</h1>
+
+        <div className="flex flex-wrap gap-8">
+          {chapters.map(chapter => (
+            <ChapterCard chapterNumber={chapter} key={chapter}/>
+          ))}
+
+          {chapters.map(chapter => (
+            <ChapterCard chapterNumber={chapter} key={chapter}/>
+          ))}
+        </div>
+      </div>
     </Navigation>
   )
 }
