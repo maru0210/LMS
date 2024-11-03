94356206f47233cb8a754575dd6a6c88e6faf27e
maru0210 <m.ryuto0210@gmail.com>
2024-11-04 02:51:40 +0900

    セクション管理コンポーネントを修正

--- src/app/(admin)/admin/SectionManager.tsx
+++ src/app/(admin)/admin/SectionManager.tsx
@@ -29,12 +29,13 @@ export default function SectionManager() {
 
   return (
     <div>
-      <button onClick={onClickHandler}>データベースに反映</button>
-      <button onClick={updateDbChapters}>データベース更新</button>
-      <div className="flex gap-8 [&>*]:flex-1">
-        <div>
-          <p className="mb-4">DBのチャプター</p>
-          <ul className="flex flex-col gap-1">
+      <h2 className="mb-4 text-xl">チャプター管理</h2>
+      {/*<button onClick={updateDbChapters}>データベース更新</button>*/}
+
+      <div className="flex gap-4">
+        <div className="flex-1">
+          <p className="mb-2 text-center">DBのチャプター</p>
+          <ul className="flex flex-col gap-1 rounded-lg p-4 shadow">
             {dbChapters.map(chapter => (
               <li key={chapter.id}>
                 {`${chapter.number}. ${chapter.title} [${chapter.id}]`}
@@ -43,9 +44,15 @@ export default function SectionManager() {
           </ul>
         </div>
 
-        <div>
-          <p className="mb-4">CMSのチャプター</p>
-          <ul className="flex flex-col gap-1">
+        <div className="flex items-center">
+          <button className="rounded-lg p-2 bg-gray-50 shadow" onClick={onClickHandler}>
+            ⇐<br/>反映<br/>⇐
+          </button>
+        </div>
+
+        <div className="flex-1">
+          <p className="mb-2 text-center">CMSのチャプター</p>
+          <ul className="flex flex-col gap-1 rounded-lg p-4 shadow">
             {cmsChapters.map(chapter => (
               <li key={chapter.id}>
                 {`${chapter.number}. ${chapter.title} [${chapter.id}]`}
@@ -54,6 +61,7 @@ export default function SectionManager() {
           </ul>
         </div>
       </div>
+
     </div>
   )
 }
