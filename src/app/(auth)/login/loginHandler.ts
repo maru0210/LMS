9b5254a2a7398912d8cf48fa00dcb253441733c0
maru0210 <m.ryuto0210@gmail.com>
2024-11-05 03:12:41 +0900

    教師用のチャットページを作成

--- src/app/(auth)/login/loginHandler.ts
+++ src/app/(auth)/login/loginHandler.ts
@@ -1,12 +1,12 @@
 "use server"
 
 import {login} from "@/app/lib/supabase/auth";
-import getErrorMessage from "@/app/lib/supabase/getErrorMessage";
+import getAuthErrorMessage from "@/app/lib/supabase/getAuthErrorMessage";
 
 export default async function loginHandler(
   prevState: string | null, formData: FormData
 ) {
   const error = await login(formData)
 
-  return getErrorMessage(error)
+  return getAuthErrorMessage(error)
 }
