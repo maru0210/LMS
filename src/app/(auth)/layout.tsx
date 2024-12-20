b0c15f57bca525c80959eb222487af7341df5e65
maru0210 <m.ryuto0210@gmail.com>
2024-12-20 19:47:27 +0900

    セッション確認を行うlayoutを追加

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,18 @@
-import {checkSession} from "@/lib/supabase/auth";
+import {getProfile} from "@/lib/supabase/profile";
+import {createClient} from "@/utils/supabase/server";
+import {redirect} from "next/navigation";
 import React from "react";
 
 export default async function AuthLayout(
-  { children }: Readonly<{ children: React.ReactNode }>
+  {children}: Readonly<{ children: React.ReactNode }>
 ) {
-  await checkSession()
+  const supabase = await createClient();
+  const {data: {user}} = await supabase.auth.getUser()
+  if (user) {
+    const profile = await getProfile(user.id);
+    if (profile.status === "student") redirect("/home");
+    else if (profile.status === "teacher") redirect("/manager/exam");
+  }
 
   return children;
 }
