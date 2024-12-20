c40d9d45288120ed870c010e6eb93682964a66a1
maru0210 <m.ryuto0210@gmail.com>
2024-12-20 19:45:41 +0900

    getUserStatusを移動, checkSessionをverifyUserStatusに変更

--- src/lib/supabase/user.ts
+++ src/lib/supabase/user.ts
@@ -1,9 +1,16 @@
+import {getProfile} from "@/lib/supabase/profile";
 import {createClient} from "@/utils/supabase/server";
 import {redirect} from "next/navigation";
 
 export async function getCurrentUser() {
   const supabase = await createClient();
   const {data, error} = await supabase.auth.getUser();
-  if (error || !data.user) redirect("/error");
+  if (error || !data.user) redirect("/login");
   return data.user;
+}
+
+export async function getUserStatus() {
+  const user = await getCurrentUser();
+  const profile = await getProfile(user.id);
+  return profile.status;
 }
