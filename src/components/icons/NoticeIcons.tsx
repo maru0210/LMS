fef84dba81aa868427f4bf65dd3508853caf8305
maru0210 <m.ryuto0210@gmail.com>
2024-12-22 20:38:01 +0900

    Processing状態用のアイコンを追加

--- src/components/icons/NoticeIcons.tsx
+++ src/components/icons/NoticeIcons.tsx
@@ -29,3 +29,62 @@ export const InfoIcon = () => (
           d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"/>
   </svg>
 )
+
+export const ProcessingIcon = () => (
+  <div className="flex items-center justify-center size-5">
+    <svg viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg" className="size-4">
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(2 1)" stroke="#FFF" strokeWidth="1.5">
+          <circle cx="42.601" cy="11.462" r="5" fillOpacity="1" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="1;0.2;0.2;0.2;0.2;0.2;0.2;0.2" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="49.063" cy="27.063" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;1;0.2;0.2;0.2;0.2;0.2;0.2;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="42.601" cy="42.663" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;0.2;1;0.2;0.2;0.2;0.2;0.2;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="27" cy="49.125" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;0.2;0.2;1;0.2;0.2;0.2;0.2;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="11.399" cy="42.663" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;0.2;0.2;0.2;1;0.2;0.2;0.2;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="4.938" cy="27.063" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;0.2;0.2;0.2;0.2;1;0.2;0.2;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="11.399" cy="11.462" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;0.2;0.2;0.2;0.2;0.2;1;0.2;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+          <circle cx="27" cy="5" r="5" fillOpacity="0" fill="#fff">
+            <animate attributeName="fill-opacity"
+                     begin="0s" dur="1.3s"
+                     values="0.2;0.2;0.2;0.2;0.2;0.2;0.2;1;" calcMode="linear"
+                     repeatCount="indefinite"/>
+          </circle>
+        </g>
+      </g>
+    </svg>
+  </div>
+)
