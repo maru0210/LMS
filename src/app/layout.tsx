8b21ecaa3d4842ce869e6fb2d6bac9c0c87f2314
maru0210 <m.ryuto0210@gmail.com>
2025-01-27 08:44:04 +0900

    フォントの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,15 @@ import localFont from "next/font/local";
 import React from "react";
 import "./globals.css";
 
-const BIZUDP = localFont({
+const MPlus2 = localFont({
   src: [
     {
-      path: "../fonts/BIZUDPGothic-Regular.woff2",
+      path: "../fonts/MPLUS2-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/BIZUDPGothic-Bold.woff2",
+      path: "../fonts/MPLUS2-Bold.ttf",
       weight: "700",
       style: "bold",
     },
@@ -24,7 +24,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ja">
-      <body className={cn(BIZUDP.className, "leading-relaxed tracking-wider")}>
+      <body className={cn(MPlus2.className, "leading-relaxed tracking-wider")}>
         <NoticeProvider>{children}</NoticeProvider>
       </body>
     </html>
