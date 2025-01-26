4621a4b7e1150dfb7c97b1bf6d8f6711630b10dc
maru0210 <m.ryuto0210@gmail.com>
2025-01-26 21:58:20 +0900

    フォントの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,30 @@
 import NoticeProvider from "@/components/notice/Notice";
+import { cn } from "@/lib/utils";
 import localFont from "next/font/local";
 import React from "react";
 import "./globals.css";
 
-const LINESeedJP = localFont({
+const BIZUDP = localFont({
   src: [
     {
-      path: '../fonts/LINESeedJP_OTF_Th.woff2',
-      weight: '100',
-      style: 'thin',
+      path: "../fonts/BIZUDPGothic-Regular.woff2",
+      weight: "400",
+      style: "normal"
     },
     {
-      path: '../fonts/LINESeedJP_OTF_Rg.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/LINESeedJP_OTF_Bd.woff2',
-      weight: '700',
-      style: 'bold',
+      path: "../fonts/BIZUDPGothic-Bold.woff2",
+      weight: "700",
+      style: "bold"
     }
   ]
-})
+});
 
 export default function RootLayout(
-  {children}: Readonly<{ children: React.ReactNode; }>
+  { children }: Readonly<{ children: React.ReactNode; }>
 ) {
   return (
     <html lang="ja">
-    <body className={[LINESeedJP.className].join(" ")}>
+    <body className={cn(BIZUDP.className, "leading-relaxed tracking-wider")}>
     <NoticeProvider>
       {children}
     </NoticeProvider>
