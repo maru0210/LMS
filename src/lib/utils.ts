27134c8ecf60cf2ff6bb822dc423fb68653bee32
maru0210 <m.ryuto0210@gmail.com>
2025-01-26 21:59:20 +0900

    関数toZenkakuを追加

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
 export function cn(...className: (string | undefined | false)[]) {
-  return className.join(" ")
+  return className.join(" ");
+}
+
+export function toZenkaku(n: number) {
+  return String(n).replace(
+    /[0-9]/g,
+    s => String.fromCharCode(Number(s) + "０".charCodeAt(0))
+  );
 }
